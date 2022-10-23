b5b59bed6b84902290b799a18a66314dd0a78ec7
SerhiiTsukanov <94555214+SerhiiTsukanov@users.noreply.github.com>
2022-10-23 18:20:37 +0300

    Statistics

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,33 @@
-export const App = () => {
+import Profile from './Profile';
+import Statistics from './Statistics';
+import user from '../user.json'; 
+import data from '../data.json';
+
+// const data = data[1];
+console.log(data);
+
+export default function App() {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    // <div
+    // >
+    //   <Profile
+    //     username={user.username}
+    //     alt={user.avatar}
+    //     tag={user.tag}
+    //     location={user.location}
+    //     followers={user.stats.followers}
+    //     views={user.stats.views}
+    //     likes={user.stats.likes}
+    //   />
+    // </div>,
+    // <div>
+    //   <Statistics
+    //     title="Upload stats"
+    //     stats={data} />
+      
+    //   </div>,
+    <div>
+        <Statistics stats={data} />
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,57 +1,61 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import user from './user.json';
+// import user from './user.json';
 // import ReactDOM from 'react-dom/client';
-// import { App } from 'components/App';
+import App from './components/App';
 // import './index.css';
+
+
+// function User({alt, username, tag, location, followers, views, likes}) {
+//   return (
+//     <div class="profile">
+//       <div class="description">
+//         <img
+//           src={alt}
+//           alt="User avatar"
+//           class="avatar"
+//         />
+//         <p class="name">{username}</p>
+//         <p class="tag">{tag}</p>
+//         <p class="location">{location}</p>
+//       </div>
+
+//       <ul class="stats">
+//         <li>
+//           <span class="label">Followers</span>
+//           <span class="quantity">{followers}</span>
+//         </li>
+//         <li>
+//           <span class="label">Views</span>
+//           <span class="quantity">{views}</span>
+//         </li>
+//         <li>
+//           <span class="label">Likes</span>
+//           <span class="quantity">{likes}</span>
+//         </li>
+//       </ul>
+//     </div>
+//   );
+// }
+
+
+
+// ReactDOM.render(
+//   <User
+//     username={user.username}
+//     alt={user.avatar}
+//     tag={user.tag}
+//     location={user.location}
+//     followers={user.stats.followers}
+//     views={user.stats.views}
+//     likes={user.stats.likes}
+//   />,
+//   document.querySelector('#root'));
+
+ReactDOM.render(<App />, document.querySelector('#root'));
 
 // ReactDOM.createRoot(document.getElementById('root')).render(
 //   <React.StrictMode>
 //     <App />
 //   </React.StrictMode>
 // );
-function User({alt, username, tag, location, followers, views, likes}) {
-  return (
-    <div class="profile">
-      <div class="description">
-        <img
-          src={alt}
-          alt="User avatar"
-          class="avatar"
-        />
-        <p class="name">{username}</p>
-        <p class="tag">{tag}</p>
-        <p class="location">{location}</p>
-      </div>
-
-      <ul class="stats">
-        <li>
-          <span class="label">Followers</span>
-          <span class="quantity">{followers}</span>
-        </li>
-        <li>
-          <span class="label">Views</span>
-          <span class="quantity">{views}</span>
-        </li>
-        <li>
-          <span class="label">Likes</span>
-          <span class="quantity">{likes}</span>
-        </li>
-      </ul>
-    </div>
-  );
-}
-
-
-
-ReactDOM.render(
-  <User
-    username={user.username}
-    alt={user.avatar}
-    tag={user.tag}
-    location={user.location}
-    followers={user.stats.followers}
-    views={user.stats.views}
-    likes={user.stats.likes}
-  />,
-  document.querySelector('#root'));
