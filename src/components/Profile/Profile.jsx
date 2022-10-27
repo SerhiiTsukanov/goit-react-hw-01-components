80b8dafb1376f3f096883f1555b174b29ebb1cf1
SerhiiTsukanov <94555214+SerhiiTsukanov@users.noreply.github.com>
2022-10-27 23:10:58 +0300

    css.

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -4,33 +4,60 @@ import css from '../Profile/Profile.css';
 
 export default function User({ alt, username, tag, location, followers, views, likes }) {
   return (
-    <div class="profile">
-      <div class="description">
+    <div className={css.profile}>
+      <div className={css.description}>
         <img
           src={alt}
           alt="User avatar"
-          class="avatar"
+          className={css.avatar}
         />
-        <p class="name">{username}</p>
-        <p class="tag">{tag}</p>
-        <p class="location">{location}</p>
+        <p className={css.name}>{username}</p>
+        <p className={css.tag}>{tag}</p>
+        <p className={css.location}>{location}</p>
       </div>
 
-      <ul class="stats">
+      <ul className={css.stats}>
         <li>
-          <span class="label">Followers</span>
-          <span class="quantity">{followers}</span>
+          <span className={css.label}>Followers</span>
+          <span className={css.quantity}>{followers}</span>
         </li>
         <li>
-          <span class="label">Views</span>
-          <span class="quantity">{views}</span>
+          <span className={css.label}>Views</span>
+          <span className={css.quantity}>{views}</span>
         </li>
         <li>
-          <span class="label">Likes</span>
-          <span class="quantity">{likes}</span>
+          <span className={css.label}>Likes</span>
+          <span className={css.quantity}>{likes}</span>
         </li>
       </ul>
     </div>
+    // <div class="profile">
+    //   <div class="description">
+    //     <img
+    //       src={alt}
+    //       alt="User avatar"
+    //       class="avatar"
+    //     />
+    //     <p class="name">{username}</p>
+    //     <p class="tag">{tag}</p>
+    //     <p class="location">{location}</p>
+    //   </div>
+
+    //   <ul class="stats">
+    //     <li>
+    //       <span class="label">Followers</span>
+    //       <span class="quantity">{followers}</span>
+    //     </li>
+    //     <li>
+    //       <span class="label">Views</span>
+    //       <span class="quantity">{views}</span>
+    //     </li>
+    //     <li>
+    //       <span class="label">Likes</span>
+    //       <span class="quantity">{likes}</span>
+    //     </li>
+    //   </ul>
+    // </div>
   );
 }
 
