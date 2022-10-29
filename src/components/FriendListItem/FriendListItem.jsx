780ed02a66c5c507b70d02d3a3606fab4acb7876
SerhiiTsukanov <94555214+SerhiiTsukanov@users.noreply.github.com>
2022-10-29 03:19:45 +0300

    1111

--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -1,17 +1,14 @@
 
 import PropTypes from 'prop-types';
-import css from '../FriendListItem/FriendListItem.module.css';
+import css from './FriendListItem.module.css';
 
 export default function FriendListItem({ friends }) {
     return (
      <ul className={css.friend_list}>
         {friends.map(friend => (
         <li className={css.item} key={friends.id}>
-            
                 <div className={css.box} >
-                    <span className={css.status} color={({ eventOnline, theme }) => {
-    return eventOnline ? theme.colors.green : theme.colors.red;
-  }}>{friend.isOnline}</span>
+              <span className={css.status_on}>{friend.isOnline}</span>
                     <img className={css.avatar} src={friend.avatar} alt="User avatar" width="48" />
                     <p className={css.name}>{friend.name}</p>  
                 </div>
@@ -32,3 +29,4 @@ FriendListItem.propTypes = {
     })
   ),
 };
+
